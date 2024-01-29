5f22022aaa4a203501ec994160cb91164001eaa3
Faxraddin <reydivonay@gmail.com>
2024-01-30 00:52:50 +0400

    align try 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,16 +5,17 @@ import RightPart from './RightPart/RightPart';
 
 const Form = () => {
   return (
-    <div style={{display:'flex'}}>
+    <div style={{ display: "flex" }}>
       <div className="main_form_div">
         <Select fields={["React", "Node"]} />
-        <hr style={{ width: "300px" }} />
-        <p className="upload_heading">
-          Select a manifest that you'd like to import
-        </p>
-        <UploadField />
+        <div style={{display:'flex',flexDirection:'column',width:'820px'}}>
+          <p className="upload_heading">
+            Select a manifest that you'd like to import
+          </p>
+          <UploadField />
+        </div>
       </div>
-      <RightPart/>
+      <RightPart />
     </div>
   );
 };
